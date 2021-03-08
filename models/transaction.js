e29460950fcbe6b09d48cebd84cab84d1f9c16fa
Michelle Smith <smith.michelle.a@gmail.com>
2021-03-08 15:09:37 -0500

    added process.env

--- models/transaction.js
+++ models/transaction.js
@@ -1,4 +1,8 @@
 const mongoose = require("mongoose");
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/transaction", {
+  useNewUrlParser: true,
+  useFindAndModify: false
+});
 
 const Schema = mongoose.Schema;
 
